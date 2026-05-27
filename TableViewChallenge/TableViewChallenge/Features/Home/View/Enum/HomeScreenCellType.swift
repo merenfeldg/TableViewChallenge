9167da2c7a47ee8ab1c8fd4353416a9b0345b902
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-05-27 14:03:06 -0300

    feat: create HomeScreenCellType enum in Features/Home/View/Enum folder

---
+++ TableViewChallenge/TableViewChallenge/Features/Home/View/Enum/HomeScreenCellType.swift
@@ -0,0 +1,15 @@
+//
+//  HomeScreenCellType.swift
+//  TableViewChallenge
+//
+//  Created by Gabriel Merenfeld on 27/05/26.
+//
+
+enum HomeScreenCellType: CaseIterable {
+    case header
+    case dailyWeatherForecast
+    case curiosityOfTheDay
+    case nearPlaces
+    case qualityOfAir
+    case upcomingEvents
+}
